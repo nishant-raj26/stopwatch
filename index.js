7104bec5cebf4c1f9fd7bbc4ac00f2ef735c1a4d
Nishant Raj <nishantraj87726@gmail.com>
2023-04-30 19:55:55 +0530

    stopwatch

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+const clock = document.querySelector('.clock')
+let sec = 0, min = 0, hr = 0, intervalId
+
+const startStopWatch = () => {
+    intervalId = setInterval(() => {
+        if (sec < 59) {
+            sec++
+        }
+        else if (min >= 59) {
+            hr++
+            min = 0
+        }
+        else {
+            sec = 0
+            min++
+        }
+
+        const seconds = String(sec).padStart(2, "0")
+        const minutes = String(min).padStart(2, "0")
+        const hours = String(hr).padStart(2, "0")
+
+       clock.innerHTML = (`${hours}: ${minutes} : ${seconds}`)
+    }, 1000);
+}
+
+const stopStopWatch = () => {
+    clearInterval(intervalId)
+}
